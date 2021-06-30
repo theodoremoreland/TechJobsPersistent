286592e11038738e5bf58039d0c4fc163668f732
Theodore Moreland <theodoremoreland@yahoo.com>
2021-06-29 23:04:33 -0500

    Added annotations to AbstractEntity for ORM validation and support.

--- src/main/java/org/launchcode/TechJobsPersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/TechJobsPersistent/models/AbstractEntity.java
@@ -1,12 +1,21 @@
 package org.launchcode.TechJobsPersistent.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotBlank
+    @Size(max = 100)
     private String name;
 
     public int getId() {
