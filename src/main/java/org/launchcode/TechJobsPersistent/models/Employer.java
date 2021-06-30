b4cb88ef8ac9ae7e3095671982dec2939a11bbbb
Theodore Moreland <theodoremoreland@yahoo.com>
2021-06-29 23:05:56 -0500

    Added @Entity + location and description field to Employer and Skill classes, respectively.

--- src/main/java/org/launchcode/TechJobsPersistent/models/Employer.java
+++ src/main/java/org/launchcode/TechJobsPersistent/models/Employer.java
@@ -1,6 +1,23 @@
 package org.launchcode.TechJobsPersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(max = 100)
+    private String location;
 
+    public Employer() { }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/TechJobsPersistent/models/Skill.java
+++ src/main/java/org/launchcode/TechJobsPersistent/models/Skill.java
@@ -1,5 +1,19 @@
 package org.launchcode.TechJobsPersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 1000)
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
